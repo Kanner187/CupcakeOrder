f851523dc6f5ea4de14986317f7d3ce1ee817d27
Levit Osei Kanner <lkanner21@gmail.com>
2019-11-18 17:07:23 +0000

    Creates CheckOut view

--- CupcakeCorner/CheckOutView.swift
+++ CupcakeCorner/CheckOutView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 
 struct CheckOutView: View {
+    @ObservedObject var order: Order
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -16,6 +17,6 @@ struct CheckOutView: View {
 
 struct CheckOutView_Previews: PreviewProvider {
     static var previews: some View {
-        CheckOutView()
+        CheckOutView(order: Order())
     }
 }
